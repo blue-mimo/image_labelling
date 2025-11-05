32916f3188a1f01c907cd44f4b6eaa89acbbddb8
Michael Monz <Michael.Monz@bluestone-tec.com>
2025-11-05 17:50:38 +0100

    Remove duplicate class definition

--- lambda/upload_image.py
+++ lambda/upload_image.py
@@ -6,13 +6,6 @@ import os
 from botocore.exceptions import ClientError
 
 
-class HTTPClientError(Exception):
-    def __init__(self, status_code, message):
-        self.status_code = status_code
-        self.message = message
-        super().__init__(message)
-
-
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
